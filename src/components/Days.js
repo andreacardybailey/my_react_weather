163845c8e5c1c88dc14aa749e7d47252d3a3c300
Andrea Cardy <acardy@customink.com>
2017-12-05 22:35:47 -0500

    Displaying state with JSX - Final

--- src/components/Days.js
+++ src/components/Days.js
@@ -3,19 +3,19 @@ import Day from './Day';
   
 class Days extends React.Component {
   render() {
+    const cityData = this.props.data.city;
     return (
       <div>
         <h1>Weather for {this.props.cityState}</h1>
         <div className="days">
-          <Day />
-          <Day />
-          <Day />
-          <Day />
-          <Day />
+          {
+            cityData.map( (day, i) => {
+              return <Day day={ day } key={ i } />
+            })
+          }
         </div>
       </div>
     )
   }
 }
 
-export default Days;
