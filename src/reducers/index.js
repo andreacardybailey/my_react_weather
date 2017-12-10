fe8af5e846e8bbee9a694ce714db933d4f05073c
Andrea Cardy <acardy@customink.com>
2017-12-10 13:09:14 -0500

    Integrating redux store with react rounter v4

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
+import { routerReducer as routing } from 'react-router-redux';
 
 import city from './city';
 import currentDay from './currentDay';
 
 
-const rootReducer = combineReducers({city, currentDay, routing: routerReducer });
+const rootReducer = combineReducers({city, currentDay, routing });
 
 export default rootReducer;
--- src/store.js
+++ src/store.js
@@ -1,19 +1,20 @@
-import { createStore, compse } from 'redux';
-import { syncHistoryWithStore} from 'react-router-redux';
-import { browserHistory } from 'react-router';
-
-// import the root reducer
+import { createStore, applyMiddleware } from 'redux';
+import { routerMiddleware} from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory'
 
 import rootReducer from './reducers/index';
-// import weather data
-import sample_city from './sample_city';
+import sample_city from './sample-city';
+
+// Create a browser history
+export const history = createHistory();
+// Build the middleware for intercepting and dispatching navigation actions
+const middleware = routerMiddleware(history);
 
 const defaultState = {
   city: sample_city,
-  currentDay: sampleCity[0]
+  currentDay: sample_city[0]
 }
 
-const store = createStore(rootReducer, defaultState);
-export const history = syncHistoryWithStore(browserHistory);
+const store = createStore(rootReducer, defaultState, applyMiddleware(middleware));
 
 export default store;
