ac0439985ce757f624f74779150e45f14bdfaaf6
Andrea Cardy <acardy@customink.com>
2017-12-10 11:19:56 -0500

    added reducers. still not connected to anything. app still using react state via props

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+
+import city from './city';
+import currentDay from './currentDay';
+
+
+const rootReducer = combineReducers({city, currentDay, routing: routerReducer });
+
+export default rootReducer;
